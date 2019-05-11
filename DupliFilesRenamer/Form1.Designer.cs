12e6892e72d16e86a1ea438cc8ef17425b0efebd
p0x0q <p0x0q.jp@gmail.com>
2019-05-11 11:32:45 +0900

    プロジェクト ファイルを追加します。

---
+++ DupliFilesRenamer/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace DulpliFilesRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1_ext = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2_replacefiles = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3_folderpath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4_replace_after_filename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1_date_format = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1_ext
+            // 
+            this.textBox1_ext.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1_ext.Location = new System.Drawing.Point(256, 131);
+            this.textBox1_ext.Name = "textBox1_ext";
+            this.textBox1_ext.Size = new System.Drawing.Size(59, 28);
+            this.textBox1_ext.TabIndex = 0;
+            this.textBox1_ext.Text = ".avi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(256, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "拡張子";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "置き換えるファイル名(ワイルドカード)";
+            // 
+            // textBox2_replacefiles
+            // 
+            this.textBox2_replacefiles.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2_replacefiles.Location = new System.Drawing.Point(12, 131);
+            this.textBox2_replacefiles.Name = "textBox2_replacefiles";
+            this.textBox2_replacefiles.Size = new System.Drawing.Size(238, 28);
+            this.textBox2_replacefiles.TabIndex = 3;
+            this.textBox2_replacefiles.Text = "*";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(173, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "実行する";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "対象のフォルダパス";
+            // 
+            // textBox3_folderpath
+            // 
+            this.textBox3_folderpath.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox3_folderpath.Location = new System.Drawing.Point(12, 30);
+            this.textBox3_folderpath.Multiline = true;
+            this.textBox3_folderpath.Name = "textBox3_folderpath";
+            this.textBox3_folderpath.Size = new System.Drawing.Size(313, 77);
+            this.textBox3_folderpath.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(11, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 32);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "フォルダパスを開く";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(12, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "置換後のファイル名";
+            // 
+            // textBox4_replace_after_filename
+            // 
+            this.textBox4_replace_after_filename.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox4_replace_after_filename.Location = new System.Drawing.Point(12, 197);
+            this.textBox4_replace_after_filename.Name = "textBox4_replace_after_filename";
+            this.textBox4_replace_after_filename.Size = new System.Drawing.Size(313, 28);
+            this.textBox4_replace_after_filename.TabIndex = 10;
+            this.textBox4_replace_after_filename.Text = "{$date}{$ext}";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(11, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 22);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "{$date}のDateFormat";
+            // 
+            // textBox1_date_format
+            // 
+            this.textBox1_date_format.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1_date_format.Location = new System.Drawing.Point(11, 249);
+            this.textBox1_date_format.Name = "textBox1_date_format";
+            this.textBox1_date_format.Size = new System.Drawing.Size(238, 28);
+            this.textBox1_date_format.TabIndex = 12;
+            this.textBox1_date_format.Text = "yyyy-MM-dd HH-mm-ss";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(12, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(317, 136);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "説明：置き換えるファイル名にワイルドカードを含めると、その部分が置換対象となります。\r\n[例]\r\nファイル名：  MOVI*\r\n形式：  _{$date}{$ex" +
+    "t}\r\n置換後： MOVI_2000-01-01 00-00-00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 470);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1_date_format);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4_replace_after_filename);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3_folderpath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2_replacefiles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1_ext);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1_ext;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2_replacefiles;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3_folderpath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4_replace_after_filename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1_date_format;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ DupliFilesRenamer/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DulpliFilesRenamer
+{
+    public partial class Form1 : Form
+    {
+        public static Hashtable config = new Hashtable();
+        public static Hashtable def = new Hashtable();
+        public Form1()
+        {
+            InitializeComponent();
+            Form1.CheckForIllegalCrossThreadCalls = false;
+
+            if (DLL.main.FileSearch("./database") == false)
+            {
+                DLL.main.ForderCreate("./database");
+            }
+
+            nao0x0.flag.debug = true;
+
+            def["replace_after_filename"] = textBox4_replace_after_filename.Text;
+            def["date_format"] = textBox1_date_format.Text;
+
+            DLL.main.appcheck(DLL.main.myappname());
+            DLL.main.FormStart("DupliFilesRenamer", DLL.main.myappname());
+            //DLL.form.Icon("Fuji", Properties.Resources.Icon, this);
+
+            string json = nao0x0.JSON.Load();
+            if (json != string.Empty)
+            {
+                config = nao0x0.JSON.ToHashtable(json);
+
+                if (config["folder_path"] != null)
+                textBox3_folderpath.Text = config["folder_path"].ToString();
+
+                if(config["replace_files"] != null)
+                textBox2_replacefiles.Text = config["replace_files"].ToString();
+
+                if (config["replace_after_filename"] != null)
+                textBox4_replace_after_filename.Text = config["replace_after_filename"].ToString();
+
+                if (config["ext"] != null)
+                textBox1_ext.Text = config["ext"].ToString();
+
+                if (config["date_format"] != null)
+                    textBox1_date_format.Text = config["date_format"].ToString();
+
+            }
+        }
+
+        public string set_replace_after_filename(string replace_after_filename,string date,string ext)
+        {
+            string result = replace_after_filename;
+            result = DLL.main.StringReplace(result, "{$date}", date);
+            result = DLL.main.StringReplace(result, "{$ext}", ext);
+            nao0x0.PC.DebugLog("set_replace_after_filename:"+result);
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string folder_path = textBox3_folderpath.Text;
+            string replace_files = textBox2_replacefiles.Text;
+            string replace_after_filename = textBox4_replace_after_filename.Text;
+            string ext = textBox1_ext.Text;
+            string date_format = textBox1_date_format.Text;
+            int total_replace_files = 0;
+
+            string[] replaces_files = replace_files.Split('|');
+
+            foreach(string get_replace_files in replaces_files)
+            {
+                string[] files = nao0x0.IO.GetFolderSubFiles(folder_path, get_replace_files + ext);
+                foreach (string getfile in files)
+                {
+                    FileInfo fi = new FileInfo(getfile);
+                    DateTime file_date = fi.LastWriteTime;
+                    string date = file_date.ToString(date_format);
+                    string ToFileName = DLL.main.StringRegexSearch(fi.Name, get_replace_files) + set_replace_after_filename(replace_after_filename, date, ext);
+                    nao0x0.PC.DebugLog("ToFileName:" + ToFileName);
+                    string ToReplacePath = folder_path + @"\" + ToFileName;
+                    
+
+                    if (DLL.main.FileSearch(ToReplacePath) == true)
+                    {
+                        string SourceFilePath = ToReplacePath;
+                        nao0x0.PC.DebugLog("同名のファイルが存在するため、連番を付与します");
+                        for (int i = 1; i <= 100; i++)
+                        {
+                            ToReplacePath = folder_path + @"\" + DLL.main.StringRegexSearch(fi.Name, get_replace_files) + set_replace_after_filename(replace_after_filename, date,"_" + i + ext);
+                            if (DLL.main.FileSearch(ToReplacePath) == false)
+                            {
+                                nao0x0.PC.DebugLog("moving:" + getfile + " => " + ToReplacePath);
+                                System.IO.File.Move(getfile, ToReplacePath);
+                                break;
+                            }
+                            else if (i == 99)
+                            {
+                                nao0x0.PC.DebugLog("連番が上限の99個を超えています。このファイルは置換をスキップします。FileName:" + SourceFilePath);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        nao0x0.PC.DebugLog("moving:" + getfile + " => " + ToReplacePath);
+                        System.IO.File.Move(getfile, ToReplacePath);
+                    }
+                }
+                total_replace_files += files.Length;
+
+            }
+
+            Hashtable setconfig = new Hashtable();
+
+            setconfig["folder_path"] = folder_path;
+
+            setconfig["replace_files"] = replace_files;
+
+            if(replace_after_filename != (string)def["replace_after_filename"])
+            setconfig["replace_after_filename"] = replace_after_filename;
+
+            setconfig["ext"] = ext;
+
+            if(date_format != (string)def["date_format"])
+            setconfig["date_format"] = date_format;
+
+            nao0x0.JSON.Save(nao0x0.JSON.ToJSON(setconfig));
+
+            MessageBox.Show(total_replace_files.ToString() + "個のファイルを置き換えました。");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(textBox3_folderpath.Text);
+        }
+    }
+}
